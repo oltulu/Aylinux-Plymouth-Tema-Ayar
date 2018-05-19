601db0201128ba41df09346074fa2bd4a13bf0c8
Aylinux <cihanalk@gmail.com>
2018-05-19 12:25:40 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,7 +22,7 @@ MainWindow::MainWindow(QWidget *parent) :
     temalar = yolum.entryList();
     QListWidgetItem *deneme=new QListWidgetItem();
     deneme->setIcon(QIcon("/usr/share/aylinux-plymouth/resimler/aylinux.png"));
-//    ui->listWidget->addItems(deneme);
+    ui->listWidget->addItems(temalar);
 
     // Add an item to list widget.
     //    QListWidgetItem *item = new QListWidgetItem();
